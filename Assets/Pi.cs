e5f0169b2ef65c9cf205baa0693814878da63efe
skytomo <skytomo221@gmail.com>
2026-09-06 17:24:04 +0900

    chore(assets): update samples

---
+++ Assets/Pi.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Pi : UdonSharpBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Pi: " + 3.14159265358979f);
+        Debug.Log("Pi: " + 3.14159265358979d);
+    }
+}
